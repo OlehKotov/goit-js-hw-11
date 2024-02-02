8f329d4a15ff3bb4d767a93c36f834f1caf4601a
OlehKotov <mail.oleg456@gmail.com>
2024-02-02 14:53:20 +0200

    'Update'

--- src/main.js
+++ src/main.js
@@ -2,59 +2,89 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import errorIcon from './img/bi_x-octagon.svg';
 
 const refs = {
   form: document.querySelector('.form'),
   gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
 };
+refs.loader.style.visibility = 'hidden';
 refs.form.addEventListener('submit', onFormSubmit);
 
 function getImagesByName(name) {
   const BASE_URL = 'https://pixabay.com/api';
   const PARAMS = `/?key=42132229-e88b92984f0d2a7001cb07c65&image_type=photo&orientation=horizontal&safesearch=true&q=${name}`;
   const url = BASE_URL + PARAMS;
-
-  return fetch(url).then(res => res.json());
-}
+   return fetch(url).then(response =>
+     response.json()).catch(error => {
+      console.log(error);
+    })   
+  }
 
 function onFormSubmit(event) {
   event.preventDefault();
+  refs.loader.style.visibility = 'visible';
   const query = event.target.elements.query.value;
+  
   getImagesByName(query).then(data => {
-    renderMarkup(data.hits);
+    refs.loader.style.visibility = 'hidden';
+    if(query === '') {
+      refs.gallery.innerHTML = "";
+      return
+    };
+    if (parseInt(data.totalHits) > 0) {
+      renderMarkup(data.hits);
+    } else {
+      refs.gallery.innerHTML = '';
+      iziToast.error({
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        backgroundColor: 'red',
+        messageColor: 'white',
+        messageSize: '16px',
+        iconColor: 'white',
+        iconUrl: errorIcon,
+        color: 'white',
+    });
+  
+    }
   });
   event.target.reset();
 }
 
-function galleryTemplate({largeImageURL, webformatURL, tags, likes, views, comments, downloads}) {
-  return `<a class="gallery-link" href="${largeImageURL}">
-        <img
-          class="gallery-image"
-          src="${webformatURL}"
-          data-source="${largeImageURL}"
-          alt="${tags}"
-          data-likes="${likes}"
-          data-views="${views}"
-          data-comments="${comments}"
-          data-downloads="${downloads}"
-        />
-      </a>
+function galleryTemplate({
+  largeImageURL,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<a class="gallery-link" href="${largeImageURL}"><img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
+  <div class="gallery-review">
+  <div class="gallery-review-item"><b>Likes</b> <span>${likes}</span></div>
+  <div class="gallery-review-item"><b>Views</b> <span>${views}</span></div>
+  <div class="gallery-review-item"><b>Comments</b> <span>${comments}</span></div>
+  <div class="gallery-review-item"><b>Downloads</b> <span>${downloads}</span></div>
+  </div></a>
     `;
 }
+
+let gallery = new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captionDelay: 250,
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
 
 function renderMarkup(images) {
   const markup = images.map(galleryTemplate).join('');
   refs.gallery.innerHTML = markup;
+  gallery.refresh();
 }
 
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
-	// Do something…
-});
 
-gallery.on('error.simplelightbox', function (e) {
-	console.log(e); // Some usefull information
-});
-// function blockAction(e) {
-//   if (e.target.nodeName !== 'A') e.preventDefault();
-// }
+
